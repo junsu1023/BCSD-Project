1d074a2082d052117d8f4fbcc8972cf8b5ccc686
junsu1023 <12jangdokg55@gmail.com>
2023-02-12 20:58:48 +0900

    data 틀 마련

---
+++ data/src/main/java/com/example/data/repository/EquipmentRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.example.data.repository
+
+import com.example.domain.model.EquipmentData
+import com.example.domain.repository.EquipmentRepository
+
+class EquipmentRepositoryImpl: EquipmentRepository {
+    override fun getEquipmentData(position: Int): EquipmentData {
+        TODO("Not yet implemented")
+    }
+
+    override fun insertEquipmentData(equipmentData: EquipmentData) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteEquipmentData(equipmentData: EquipmentData) {
+        TODO("Not yet implemented")
+    }
+}

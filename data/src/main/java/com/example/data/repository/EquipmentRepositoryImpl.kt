b93280e6db74d30df3df116cf23482bf24b747a4
junsu1023 <12jangdokg55@gmail.com>
2023-02-15 18:30:55 +0900

    data, presentation의 viewmodel 작성

--- data/src/main/java/com/example/data/repository/EquipmentRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/EquipmentRepositoryImpl.kt
@@ -1,18 +1,22 @@
 package com.example.data.repository
 
+import com.example.data.dao.EquipmentDao
+import com.example.data.mapper.mapToData
+import com.example.data.mapper.mapToEntity
 import com.example.domain.model.EquipmentData
 import com.example.domain.repository.EquipmentRepository
 
-class EquipmentRepositoryImpl: EquipmentRepository {
-    override fun getEquipmentData(position: Int): EquipmentData {
-        TODO("Not yet implemented")
-    }
-
+class EquipmentRepositoryImpl(private val equipmentDao: EquipmentDao): EquipmentRepository {
     override fun insertEquipmentData(equipmentData: EquipmentData) {
-        TODO("Not yet implemented")
+        equipmentDao.insertEquipmentData(equipmentData.mapToEntity())
     }
 
     override fun deleteEquipmentData(equipmentData: EquipmentData) {
-        TODO("Not yet implemented")
+        equipmentDao.deleteEquipmentData(equipmentData.mapToEntity())
+    }
+
+    override fun getEquipmentData(position: Int): EquipmentData {
+        val data = equipmentDao.getEquipmentData(position)
+        return data.mapToData()
     }
 }

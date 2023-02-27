47094545d36aede9f7fa63c07a1779014df2cc1c
hsgo2430 <hsgo2430@naver.com>
2023-02-27 20:17:13 +0900

    로그인 화면 적용

---
+++ app/src/main/java/com/example/myapplication/activity/SelectButtonActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.R
+import com.example.myapplication.adapter.EquipmentListAdapter
+import com.example.myapplication.databinding.ActivitySelectButtonBinding
+
+class SelectButtonActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectButtonBinding
+    private lateinit var changeActivity: Intent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_select_button)
+
+        val mainRentalButton: Button = binding.mainRentalButton
+        val mainUpdateButton: Button = binding.mainUpdateButton
+        val mainSearchButton: Button = binding.mainSearchButton
+
+        mainRentalButton.setOnClickListener {
+            onMainRentalButtonClick()
+        }
+
+        mainUpdateButton.setOnClickListener {
+            onMainUpdateButtonClick()
+        }
+
+        mainSearchButton.setOnClickListener {
+            onMainSearchButtonClick()
+        }
+    }
+
+    private fun onMainRentalButtonClick() {
+        changeActivity = Intent(this, SignInActivity::class.java)
+        startActivity(changeActivity)
+    }
+
+    private fun onMainUpdateButtonClick() {
+        changeActivity = Intent(this, UpdateActivity::class.java)
+        startActivity(changeActivity)
+    }
+
+    private fun onMainSearchButtonClick() {
+        changeActivity = Intent(this, SearchActivity::class.java)
+        startActivity(changeActivity)
+    }
+}
